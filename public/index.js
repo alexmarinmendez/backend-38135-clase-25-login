d476b97bc0a2f598a18623a2f366854a26270d60
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-17 17:34:01 -0500

    feat: add index script in the register form

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
-console.log('Hola Mundo')
+let submitBtn = document.getElementById('submitBtn')
+let form = document.getElementById('formRegister')
+
+submitBtn.addEventListener('click', evt => {
+    evt.preventDefault()
+    let obj = {}
+    let data = new FormData(form)
+    data.forEach((value, key) => obj[key] = value)
+    console.log(obj)
+})
